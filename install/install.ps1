4aa6738c1feb841c542b4808da43b01c9b9d1afb
Amin Ziagham <amin.ziagham@outlook.com>
2026-05-21 22:54:16 +0200

    Add install.ps1 script for automated VectraCtl setup
    Introduce a PowerShell script to automate VectraCtl CLI installation on Windows. The script handles version selection, download, extraction, PATH updates, and supports GitHub authentication and error handling.
    
    #22

---
+++ install/install.ps1
@@ -0,0 +1,145 @@
+param (
+    [string]$Version,
+    [string]$VectraCtlRootPath = "$Env:SystemDrive\vectractl"
+)
+
+Write-Output ""
+$ErrorActionPreference = 'stop'
+
+#Escape space of VectraCtlRoot path
+$VectraCtlRootPath = $VectraCtlRootPath -replace ' ', '` '
+
+# Constants
+$VectraCtlFileName = "vectractl.exe"
+$VectraCtlFilePath = "${VectraCtlRootPath}\${VectraCtlFileName}"
+
+# GitHub Org and repo hosting VectraCtl
+$GitHubOrg = "cortexiumlabs"
+$GitHubRepo = "vectractl"
+
+# Set Github request authentication for basic authentication.
+if ($Env:GITHUB_USER) {
+    $basicAuth = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($Env:GITHUB_USER + ":" + $Env:GITHUB_TOKEN));
+    $githubHeader = @{"Authorization" = "Basic $basicAuth" }
+}
+else {
+    $githubHeader = @{}
+}
+
+if ((Get-ExecutionPolicy) -gt 'RemoteSigned' -or (Get-ExecutionPolicy) -eq 'ByPass') {
+    Write-Output "PowerShell requires an execution policy of 'RemoteSigned'."
+    Write-Output "To make this change please run:"
+    Write-Output "'Set-ExecutionPolicy RemoteSigned -scope CurrentUser'"
+    break
+}
+
+# Change security protocol to support TLS 1.2 / 1.1 / 1.0 - old powershell uses TLS 1.0 as a default protocol
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+
+# Check if VectraCtl is installed.
+if (Test-Path $VectraCtlFilePath -PathType Leaf) {
+    Write-Warning "VectraCtl is detected - $VectraCtlFilePath"
+    Invoke-Expression "$VectraCtlFilePath version"
+    Write-Output "Reinstalling VectraCtl..."
+}
+else {
+    Write-Output "Installing VectraCtl..."
+}
+
+# Create VectraCtl Directory
+Write-Output "Creating $VectraCtlRootPath directory"
+New-Item -ErrorAction Ignore -Path $VectraCtlRootPath -ItemType "directory"
+if (!(Test-Path $VectraCtlRootPath -PathType Container)) {
+    Write-Warning "Please visit https://github.com/cortexiumlabs/vectractl for instructions on how to install without admin rights."
+    throw "Cannot create $VectraCtlRootPath"
+}
+
+# Get the list of release from GitHub
+$releaseJsonUrl = "https://api.github.com/repos/${GitHubOrg}/${GitHubRepo}/releases"
+
+$releases = Invoke-RestMethod -Headers $githubHeader -Uri $releaseJsonUrl -Method Get
+if ($releases.Count -eq 0) {
+    throw "No releases from github.com/cortexiumlabs/vectractl repo"
+}
+
+# Get latest or specified version info from releases
+function GetVersionInfo {
+    param (
+        [string]$Version,
+        $Releases
+    )
+    # Filter windows binary and download archive
+    if (!$Version) {
+        $release = $Releases | Where-Object { $_.tag_name -notlike "*rc*" } | Select-Object -First 1
+    }
+    else {
+        $release = $Releases | Where-Object { $_.tag_name -eq "v$Version" } | Select-Object -First 1
+    }
+
+    return $release
+}
+
+# Get info about windows asset from release
+function GetWindowsAsset {
+    param (
+        $Release
+    )
+    $windowsAsset = $Release | Select-Object -ExpandProperty assets | Where-Object { $_.name -Like "*windows-x64.zip" }
+    if (!$windowsAsset) {
+        throw "Cannot find the windows VectraCtl binary"
+    }
+    [hashtable]$return = @{}
+    $return.url = $windowsAsset.url
+    $return.name = $windowsAsset.name
+    return $return
+
+}
+
+$release = GetVersionInfo -Version $Version -Releases $releases
+if (!$release) {
+    throw "Cannot find the specified VectraCtl binary version"
+}
+$asset = GetWindowsAsset -Release $release
+$zipFileUrl = $asset.url
+$assetName = $asset.name
+
+$zipFilePath = $VectraCtlRootPath + "\" + $assetName
+Write-Output "Downloading $zipFileUrl ..."
+
+$githubHeader.Accept = "application/octet-stream"
+$oldProgressPreference = $progressPreference;
+$progressPreference = 'SilentlyContinue';
+Invoke-WebRequest -Headers $githubHeader -Uri $zipFileUrl -OutFile $zipFilePath
+$progressPreference = $oldProgressPreference;
+if (!(Test-Path $zipFilePath -PathType Leaf)) {
+    throw "Failed to download FlowCtl binary - $zipFilePath"
+}
+
+# Extract VectraCtl to $VectraCtlRootPath
+Write-Output "Extracting $zipFilePath..."
+Microsoft.Powershell.Archive\Expand-Archive -Force -Path $zipFilePath -DestinationPath $VectraCtlRootPath
+if (!(Test-Path $VectraCtlFilePath -PathType Leaf)) {
+    throw "Failed to download VectraCtl archive - $zipFilePath"
+}
+
+# Check the VectraCtl version
+# Invoke-Expression "$VectraCtlFilePath version"
+
+# Clean up zipfile
+Write-Output "Clean up $zipFilePath..."
+Remove-Item $zipFilePath -Force
+
+# Add VectraCtlRootPath directory to User Path environment variable
+Write-Output "Try to add $VectraCtlRootPath to User Path Environment variable..."
+$UserPathEnvironmentVar = [Environment]::GetEnvironmentVariable("PATH", "User")
+if ($UserPathEnvironmentVar -like '*vectractl*') {
+    Write-Output "Skipping to add $VectraCtlRootPath to User Path - $UserPathEnvironmentVar"
+}
+else {
+    [System.Environment]::SetEnvironmentVariable("PATH", $UserPathEnvironmentVar + ";$VectraCtlRootPath", "User")
+    $UserPathEnvironmentVar = [Environment]::GetEnvironmentVariable("PATH", "User")
+    Write-Output "Added $VectraCtlRootPath to User Path - $UserPathEnvironmentVar"
+}
+
+Write-Output "`r`nVectraCtl is installed successfully."
+Write-Output "To get started with VectraCtl, please visit https://github.com/cortexiumlabs/vectractl."
